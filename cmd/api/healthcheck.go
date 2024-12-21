7999634f7aad69b86d0fe888f6d43009336a315e
Gary Clarke <garyclarke1974@gmail.com>
2024-12-21 08:50:12 +0000

    Initial commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Declare a handler which writes a plain-text response with information about the
+// application status, operating environment and version.
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a map which holds the information that we want to send in the response.
+	// Declare an envelope map containing the data for the response. Notice that the way
+	// we've constructed this means the environment and version data will now be nested
+	// under a system_info key in the JSON response.
+	envelope := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, envelope, nil)
+	if err != nil {
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
+	}
+}

56c03c1c54bde0377ae06c293d6071756181c1d1
Gary Clarke <gary.clarke@immediate.co.uk>
2025-01-17 07:31:15 +0000

    Add intercepting signals

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"github.com/garyclarke/greenlight/internal/data"
 	"github.com/joho/godotenv"
 	"log"
 	"log/slog"
-	"net/http"
 	"os"
 	"time"
 
@@ -126,22 +124,8 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	// Declare a HTTP server which listens on the port provided in the config struct,
-	// uses the servemux we created above as the handler, has some sensible timeout
-	// settings and writes any log messages to the structured logger at Error level.
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
-	}
-
-	// Start the HTTP server.
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	err = srv.ListenAndServe()
+	// Call app.serve() to start the server.
+	err = app.serve()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
